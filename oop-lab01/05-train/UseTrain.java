47813887f2b0c4447a87b2bc97f4722e5e984d26
unknown <sbrunelli01@gmail.com>
2024-09-23 18:09:09 +0200

    Done

--- oop-lab01/05-train/UseTrain.java
+++ oop-lab01/05-train/UseTrain.java
@@ -1,3 +1,61 @@
+class train{
+    int nTotSeat;
+    int nFirstClassSeat;
+    int nSecondClassSeats;
+    int nFirstClassReservedSeats;
+    int nSecondClassReservedSeats;
+
+    void build(int nt, int nfc, int nsc, int rfc, int rsc){
+        this.nTotSeat = nt;
+        this.nFirstClassSeat = nfc;
+        this.nSecondClassSeats = nsc;
+        this.nFirstClassReservedSeats = rfc;
+        this.nSecondClassReservedSeats = rsc;
+    }
+
+    void reserveFirstClassSeats(int n){
+        if(this.nFirstClassReservedSeats + n <= this.nFirstClassSeat){
+            this.nFirstClassReservedSeats = this.nFirstClassReservedSeats + n;
+        }
+    }
+
+    void reserveSecondClassSeats(int n){
+        if(this.nSecondClassReservedSeats + n <= this.nSecondClassSeats){
+            this.nSecondClassReservedSeats = this.nSecondClassReservedSeats + n;
+        }
+    }
+
+    double getTotOccupancyRatio(){
+        return (((double)this.nFirstClassReservedSeats + (double)this.nSecondClassReservedSeats) / ((double)this.nFirstClassSeat + (double)this.nSecondClassSeats)) * 100;
+    }
+
+    double getFirstClassOccupancyRatio(){
+        return ((double)this.nFirstClassReservedSeats / (double)this.nFirstClassSeat) * 100;
+        
+    }
+
+    double getSecondClassOccupancyRatio(){
+        return ((double)this.nSecondClassReservedSeats / (double)this.nSecondClassSeats) * 100;
+    }
+
+    void deleteAllReservations(){
+        this.nFirstClassReservedSeats = 0;
+        this.nSecondClassReservedSeats = 0;
+    }
+    
+    void printAll(){
+        System.out.println("Total Seats: " + nTotSeat);
+        System.out.println("First Class Seats: " + nFirstClassSeat);
+        System.out.println("First Class Reserved Seats: " + nFirstClassReservedSeats);
+        System.out.println("Second Class Seats: " + nSecondClassSeats);
+        System.out.println("Second Class Reserved Seats: " + nSecondClassReservedSeats);
+        System.out.println("Total occupation:" + this.getTotOccupancyRatio() + "%");
+        System.out.println("Total 1st occupation:" + this.getFirstClassOccupancyRatio() + "%");
+        System.out.println("Total 2nd occupation:" + this.getSecondClassOccupancyRatio() + "%\n");
+
+    }
+}
+
 class UseTrain {
     public static void main(String[] args) {
         /*
@@ -24,5 +82,16 @@ class UseTrain {
          * 5) Prenotare nuovamente dei posti e stampare le nuove percentuali di
          * occupazione
          */
+        train wagon = new train();
+
+        wagon.build(100, 20, 80, 9, 70);
+        wagon.printAll();
+        wagon.reserveFirstClassSeats(1);
+        wagon.reserveSecondClassSeats(10);
+        wagon.printAll();
+        wagon.deleteAllReservations();
+        wagon.printAll();
+
+
     }
 }
