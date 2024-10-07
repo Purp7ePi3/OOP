42a9d2561bee5698ae102e00a7cbdb107233a785
unknown <sbrunelli01@gmail.com>
2024-10-07 10:56:56 +0200

    Done

--- oop-lab03/33-encapsulation/src/main/java/it/unibo/encapsulation/Smartphone.java
+++ oop-lab03/33-encapsulation/src/main/java/it/unibo/encapsulation/Smartphone.java
@@ -9,16 +9,16 @@ public class Smartphone {
     private static final int DEF_RAM_SIZE = 8192;
     private static final int DEF_N_CPU = 8;
 
-    public final int cpuCount;
-    public final int ram;
-    public final int sdSize;
-    public final String brand;
-    public final String model;
-    public final boolean hasGPS;
-    public final boolean has3G;
-    public final boolean hasNFC;
+    private final int cpuCount;
+    private final int ram;
+    private final int sdSize;
+    private final String brand;
+    private final String model;
+    private final boolean hasGPS;
+    private final boolean has3G;
+    private final boolean hasNFC;
 
-    public Smartphone(
+    private Smartphone(
         final int cpuCount,
         final int ram,
         final int sdSize,
@@ -38,23 +38,23 @@ public class Smartphone {
         this.hasNFC = hasNFC;
     }
 
-    public Smartphone(final String brand, final String model) {
+    private Smartphone(final String brand, final String model) {
         this(brand, model, DEF_SD_SIZE);
     }
 
-    public Smartphone(final String brand, final String model, final int sdSize) {
+    private Smartphone(final String brand, final String model, final int sdSize) {
         this(DEF_N_CPU, DEF_RAM_SIZE, sdSize, brand, model, DEF_HAS_GPS, DEF_HAS_3G, DEF_HAS_NFC);
     }
 
-    public Smartphone(final String brand, final String model, final boolean hasNFC) {
+    private Smartphone(final String brand, final String model, final boolean hasNFC) {
         this(DEF_N_CPU, DEF_RAM_SIZE, DEF_SD_SIZE, brand, model, DEF_HAS_GPS, DEF_HAS_3G, hasNFC);
     }
 
-    public Smartphone(final String brand, final String model, final boolean hasGPS, final boolean has3G) {
+    private Smartphone(final String brand, final String model, final boolean hasGPS, final boolean has3G) {
         this(DEF_N_CPU, DEF_RAM_SIZE, DEF_SD_SIZE, brand, model, hasGPS, has3G, DEF_HAS_NFC);
     }
 
-    public void printStringRep() {
+    private void printStringRep() {
         System.out.println("n CPU(s): " + cpuCount);
         System.out.println("RAM amount: " + ram);
         System.out.println("SD size: " + sdSize);
