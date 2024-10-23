75d69759159d34c379daa47636aff242b43bd7cb
unknown <sbrunelli01@gmail.com>
2024-10-23 11:08:22 +0200

    Done

--- oop-lab05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ oop-lab05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,7 @@
 package it.unibo.collections.sets;
 
 import java.util.Collection;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -24,22 +25,45 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+            TreeSet<String> treeSet = new TreeSet<>();
+
         /*
          * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
          * "20" (both included)
          */
+        for(int i = 1; i <= ELEMS; i++){
+            treeSet.add(String.valueOf(i));
+        }
         /*
          * 3) Prints its content
          */
+        System.out.println(treeSet);
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+        TreeSet<String> ToRemove = new TreeSet<>();
+
+        for(String c : treeSet){
+            int value = Integer.parseInt(c);
+            if(value % 3 == 0){
+                ToRemove.add(c);
+            }
+        }
+        treeSet.removeAll(ToRemove);
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+        System.out.println(treeSet);
+
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+        for (String c : treeSet) {
+            int num = Integer.parseInt(c);
+            if(num % 3 == 0){
+                System.out.println("Sono pari");
+            }
+        }
     }
 }
