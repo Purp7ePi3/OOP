b51dce89bf3c26bdea206858195b21243a93eb3b
unknown <sbrunelli01@gmail.com>
2024-10-23 11:45:43 +0200

    Done

--- oop-lab04/42-abstract-classes/src/main/java/it/unibo/inheritance/test/TestBankAccount.java
+++ oop-lab04/42-abstract-classes/src/main/java/it/unibo/inheritance/test/TestBankAccount.java
@@ -2,13 +2,14 @@ package it.unibo.inheritance.test;
 
 import it.unibo.inheritance.api.AccountHolder;
 import it.unibo.inheritance.api.BankAccount;
+import it.unibo.inheritance.impl.ClassicBankAccount;
 import it.unibo.inheritance.impl.ExtendedStrictBankAccount;
 import it.unibo.inheritance.impl.SimpleBankAccount;
 
 public class TestBankAccount {
     private static final int INTIAL_AMOUNT = 10000;
     private static final int WITHDRAW_AMOUNT = 15000;
-    private static final String ACCOUNT = "'s account balance is ";
+    //private static final String ACCOUNT = "'s account balance is ";
 
     private TestBankAccount() {
     }
@@ -61,14 +62,16 @@ public class TestBankAccount {
     public static void main(final String[] args) {
         final AccountHolder aRossi = new AccountHolder("Andrea", "Rossi", 1);
         final AccountHolder aBianchi = new AccountHolder("Alex", "Bianchi", 2);
+        final AccountHolder aVerdi = new AccountHolder("Andrea", "Verdi",3);
+        final AccountHolder atozzi = new AccountHolder("Aldo", "tozzi", 4);
         /*
          * Change the two accounts to verify that the behaviour of restricted and simple bank account are preserved
          */
-        final BankAccount restricted = null;
+        final BankAccount restricted = new ExtendedStrictBankAccount(atozzi, 0);
         final BankAccount extendedStrict = new ExtendedStrictBankAccount(aBianchi, 0);
         final BankAccount simpleBankAccount = new SimpleBankAccount(aRossi, 0);
-        final BankAccount classicBankAccount = null;
-        requireAccountsToWorkTheSameWay(restricted, extendedStrict);
+        final BankAccount classicBankAccount = new ClassicBankAccount(aVerdi,0);
+        requireAccountsToWorkTheSameWay(restricted, extendedStrict); //Questo va dio cane
         requireAccountsToWorkTheSameWay(simpleBankAccount, classicBankAccount);
     }
 }
