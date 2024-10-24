00990c707945a993b087fc9f5153be21a8589aec
unknown <sbrunelli01@gmail.com>
2024-10-24 09:46:16 +0200

    Done

--- oop-lab05/52-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
+++ oop-lab05/52-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
@@ -3,6 +3,8 @@ package it.unibo.collections.comparators;
 import java.util.Set;
 import java.util.TreeSet;
 
+import it.unibo.collections.comparators.order.StringToDoubleComparator;
+
 /**
  * 
  */
@@ -25,12 +27,20 @@ public final class UseSetWithOrder {
          * The comparator must convert the strings to double, then compare the doubles to find the biggest.
          * The comparator does not need to deal with the case of Strings which are not parseable as doubles.
          */
-        /*
-         * 2) Inserts in the set a hundred strings containing randomly generated numbers
-         * (use Math.random(), and convert to String appropriately)
-         */
-        /*
-         * 3) Prints the set, which must be ordered
-         */
+
+        TreeSet<String> treeSet = new TreeSet<>();
+
+        for (int i = 0; i < 15; i++) {
+            int randomValue = (int) (Math.random() * 101);
+            treeSet.add(Integer.toString(randomValue));
+        }
+
+        TreeSet<String> order = new TreeSet<>(new StringToDoubleComparator());
+
+        order.addAll(treeSet); // Aggiunge gli stessi elementi
+        
+        // Stampa del set ordinato
+        System.out.println("Elementi non ordinati: " + treeSet);
+        System.out.println("Elementi che ordinati: " + order);
     }
 }
